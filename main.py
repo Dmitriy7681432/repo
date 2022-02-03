6da7d0b5452fb0099b612b0792b14ba0d3656bef
Dmitriy7681432 <dima.aleksin.93@mail.ru>
2022-02-03 16:54:19 +0300

    Добавил в main.py задачу 4, пока на стадии решения

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 f = open('text.txt','r', encoding='UTF-8')
-t = f.read()
-a = t
+text = f.read()
 f.close()
 
 #Задача 1
 znak = ['-',',','«','»','.','?','!','—',';',':']
 
 for i in znak:
-     t = t.replace(i,'')
-print(t)
+     text = text.replace(i,'')
+print(text)
 
+#Задача 2
+list_text = list(text.split())
+print(list_text)
+
+#Задача 3
+map_text = list(map(str.lower,list_text))
+print(map_text)
+
+#Задача 4
+from collections import Counter
+ls = ['hello','say','pow','hello','say']
+dict_text = dict.fromkeys(ls,2)
+c = Counter(ls)
+print(c.items())
+# for i in ls:
+#      print(ls.count(i))
+# print(dict_text)
