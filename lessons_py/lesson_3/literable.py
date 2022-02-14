ceb9aafa23c625fde9a14859b8032d0502770f6c
Dmitriy7681432 <dima.aleksin.93@mail.ru>
2022-02-14 16:55:50 +0300

    Задача 4 урока 4 в процессе

--- lessons_py/lesson_3/literable.py
+++ lessons_py/lesson_3/literable.py
@@ -1,9 +1,10 @@
-f = open('C:/Users/Дмитрий/PycharmProjects/project01/lessons_py/lesson_3/text.txt', 'r', encoding='UTF-8')
-# f = open('../lessons_py/lesson_3/text.txt', 'r', encoding='UTF-8')
+# f = open('C:/Users/Дмитрий/PycharmProjects/project01/lessons_py/lesson_3/text.txt', 'r', encoding='UTF-8')
+f = open('../lesson_3/text.txt', 'r', encoding='UTF-8')
 text = f.read()
 f.close()
 
 #Задача 1
+# 1) методами строк очистить текст от знаков препинания;
 print('Задача 1')
 znak = [',','«','»','.','?','!','—',';',':']
 for i in znak:
@@ -11,16 +12,19 @@ for i in znak:
 print(text)
 
 #Задача 2
+# 2) сформировать list со словами (split);
 print('Задача 2')
 list_text = list(text.split())
 print(list_text)
 
 #Задача 3
+# 3) привести все слова к нижнему регистру (map);
 print('Задача 3')
 map_text = list(map(str.lower,list_text))
 print(map_text)
 
 #Задача 3
+# 3) получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте;
 # ls = ['hello','say','pow','hello','say']
 # dict_text = dict.fromkeys(ls,2)
 print('Задача 3')
@@ -28,6 +32,7 @@ dict_text = dict((x,map_text.count(x)) for x in map_text)
 print(dict_text)
 
 #Задача 4
+# 4) вывести 5 наиболее часто встречающихся слов (sort), вывести количество разных слов в тексте (set).
 print('Задача 4')
 list_dict_text = list(dict_text.items())
 print(list_dict_text)
@@ -53,6 +58,7 @@ print(len(set_list_text))
 # print(len(e))
 
 #Задача 5
+# 5) выполнить light с условием: в пункте 2 дополнительно к приведению к нижнему регистру выполнить лемматизацию.
 print('Задача 5')
 import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
--- lessons_py/lesson_4/func.py
+++ lessons_py/lesson_4/func.py
@@ -1,5 +1,9 @@
 import random
 #Задача 1
+# 1. Напишите функцию (F): на вход список имен и целое число N;
+# на выходе список длины N случайных имен из первого списка
+# (могут повторяться, можно взять значения: количество имен 20, N = 100,
+# рекомендуется использовать функцию random);
 print('Задача 1')
 def func (items_name,n):
     c = []
@@ -14,15 +18,8 @@ print(len(func_output))
 print('-'*100)
 
 #Задача 2
+# 2. Напишите функцию вывода самого частого имени из списка на выходе функции F;
 print('Задача 2')
-# count_func = dict((x,func_output.count(x)) for x in func_output)
-# print(count_func)
-# list_count_func = list(count_func.items())
-# list_count_func.sort(key=lambda i:i[1],reverse=True)
-# print(list_count_func)
-# for i in list_count_func[:1]:
-#     print(i[0])
-# print('func-----------------------------------')
 def func_list(list_func):
     count_func = dict((x, list_func.count(x)) for x in list_func)
     print(count_func)
@@ -34,38 +31,67 @@ def func_list(list_func):
 
 func1 = func_list(func_output)
 print(func1)
+print('-'*50)
+#Задача 3
+# 3. Напишите функцию вывода самой редкой буквы, с которого начинаются имена в списке на выходе функции F.
+print('Задача 3')
+
+def func_simvol(a):
+    sl = []
+    # for i in a:
+    for i in func_output:
+        # for j in i:
+        sl.append(i[0])
+    # print(sl)
+    ls1 = dict((x, sl.count(x)) for x in sl)
+    ls2 = list(ls1.items())
+    ls2.sort(key=lambda i:i[1])
+    print(ls2)
+    for i in ls2[:1]:
+        # print(i[0])
+        return i[0]
+ds = func_simvol(func_output)
+print(ds)
+
+#Задача 4
+# 4.  В файле с логами найти дату самого позднего лога (по метке времени): https://drive.google.com/open?id=1pKGu-u2Vvtx4xK8i2ZhOzE5rBXyO4qd8
+
+f = open('log','r',encoding='utf-8')
+a = f.read()
+print(a)
+
+b = a.split('\n')
+print(b)
+c = sorted(b,reverse=True)
+print(c[0])
+d=[]
+for i in c[0]:
+    if i.isalpha():
+        break
+    d.append(i)
+print(''.join(d)[:-2])
+f.close()
+
+e = [1,2,3,4,5,6,7,8,9,0]
+v = list(e)
+print(v)
 
 
+# a = '2011'
+# b = sorted(a,reverse=True)
+# c =''.join(b)
+# print(c)
+
+
+
+
+
+
+# with open('log', mode='rt', encoding='utf-8') as f:
+#     text = f.read()
+# print(text, '\n')
 #
-# import random
-#
-# print()
-# print("Задача 1. \nВыбор 100 случайных имен из списка")
-# # 20 имен
-# list_names = 'Natalia Ksenia Viola Ivan Kostya Kosmos Katya Darya Valya Nikita Serezha Vitya Sarah Petya Victor Seva Lyonya Maxim Vova Raya'.split()
-#
-#
-# def name_n(list_names, n):
-#     '''
-#     :param list_names: список имен
-#     :param n: длина списка на выходе
-#     :return: список из 20 случайных имен
-#     '''
-#     list_names_random = []
-#     len_list = len(list_names)
-#     newName = ''
-#
-#     for i in range(1, n + 1):
-#         # randomChoice - это случайное число в пределах длины начального списка,
-#         # случайным образом выбираем индекс
-#         randomChoice = random.randint(0, len_list-1)
-#         # newName - это случайное слово по индексу
-#         newName = list_names[randomChoice]
-#         # Добавляем случайное слово newName в список
-#         list_names_random.append(newName)
-#
-#     return list_names_random
-#
-# list_names_random = name_n(list_names, 100) # n == 100
-# print('Список: \n%s\nКоличество слов: %s\nИзначально слов: %s' % (list_names_random, len(list_names_random), len(list_names)))
-# print()
+# text = text.split('\n')
+# last_log = max(text, key=lambda x: x[:23])
+# print('Cамый поздний лог:', last_log)
+# print('И его метка времени:', last_log[:23])
