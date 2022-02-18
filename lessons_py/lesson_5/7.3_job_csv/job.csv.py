9af75f2b1dfb7dc944b8216ba5532c72c60f3da2
Dmitriy7681432 <dima.aleksin.93@mail.ru>
2022-02-18 13:05:32 +0300

    Прошел раздел 7, остается сделать ДЗ

--- lessons_py/lesson_5/7.3_job_csv/job.csv.py
+++ lessons_py/lesson_5/7.3_job_csv/job.csv.py
@@ -16,8 +16,25 @@ with open('example.csv','r') as f:
 
 #csv.Dictwriter
 data_dict = [{'Name': 'Dima', 'age':28},
-            {'Name': 'Kate', 'age':29},
+            {'age':29, 'Name': 'Kate'},
             {'Name': 'Mike', 'age':31}]
 field_names= ['Name','age']
 
+with open('example_1.csv','w') as f:
+    writer = csv.DictWriter(f,delimiter = '&',fieldnames=field_names)
+    writer.writeheader()
+    for i in range(len(data_dict)):
+        writer.writerow((data_dict[i]))
 
+
+#csv.Dictreader
+with open('example_1.csv') as f:
+    reader = csv.DictReader(f,delimiter = '&')
+    for row in reader:
+        print(row)
+
+import pandas as pd
+
+analize_csv = pd.read_csv('example_1.csv',sep='&')
+print(type(analize_csv))
+print(analize_csv)
