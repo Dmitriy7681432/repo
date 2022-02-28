8f03a52190a1627ce8b41704683d119f640b97c1
Aleks <dima.aleksin.93@mail.ru>
2022-03-01 00:11:04 +0300

    В процессе дом задание урока 8

--- lessons/lesson_8/decorator.py
+++ lessons/lesson_8/decorator.py
@@ -1,45 +1,45 @@
-# Простой декоратор
-def show_information(f):
-    def wrapper(*args, **kwargs):
-        print('Код до функции!')
-        f(*args, **kwargs)
-        print('Код после функции!')
-    return wrapper
-
-def simple_function():
-    print('Я простая функция')
-
-@show_information
-def another_simple_function():
-    print('Я тоже простая функция')
-
-simple_function()
-
-simple_function_decorated = show_information(simple_function)
-
-simple_function_decorated()
-
-another_simple_function()
-
-# Декораторы с параметрами
-
-
-def show_type(f):
-
-    def wrapper(*args, **kwargs):
-        print('Код до функции!', type(args[0]))
-        print(f(*args, **kwargs))
-        print('Код после функции!', type(args[1]))
-    return wrapper
-
-
-@show_information
-@show_type
-def my_add(a,b):
-    return a+b
-
-my_add(10,20)
-
+# # Простой декоратор
+# def show_information(f):
+#     def wrapper(*args, **kwargs):
+#         print('Код до функции!')
+#         f(*args, **kwargs)
+#         print('Код после функции!')
+#     return wrapper
+#
+# def simple_function():
+#     print('Я простая функция')
+#
+# @show_information
+# def another_simple_function():
+#     print('Я тоже простая функция')
+#
+# simple_function()
+#
+# simple_function_decorated = show_information(simple_function)
+#
+# simple_function_decorated()
+#
+# another_simple_function()
+#
+# # Декораторы с параметрами
+#
+#
+# def show_type(f):
+#
+#     def wrapper(*args, **kwargs):
+#         print('Код до функции!', type(args[0]))
+#         print(f(*args, **kwargs))
+#         print('Код после функции!', type(args[1]))
+#     return wrapper
+#
+#
+# @show_information
+# @show_type
+# def my_add(a,b):
+#     return a+b
+#
+# my_add(10,20)
+#
 
 # Пример использования декоратора
 
