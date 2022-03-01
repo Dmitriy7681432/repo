2a041e5da4138371e5d8a850d9ee0a5d26e62b99
Dmitriy7681432 <dima.aleksin.93@mail.ru>
2022-03-01 17:48:16 +0300

    В процессе дз по уроку 8

--- lessons/lesson_8/dz.py
+++ lessons/lesson_8/dz.py
@@ -1,32 +1,37 @@
-import time
+# import time
+# import random
+# import sys
+#
+# def show(f):
+#     def func(*args,**kwargs):
+#         start_time = time.time()
+#         print(start_time)
+#         f(*args,**kwargs)
+#         time.sleep(3.245)
+#         stop_time = time.time()
+#         print(stop_time)
+#         print(f'Время выполнения функции:{stop_time-start_time}')
+#     return func
+#
+# @show
+# def func1():
+#     a = ['Volvo','BMW','Audio','Lada','Lexus']
+#     b =[]
+#     c =[]
+#     for i in a:
+#         b.append(i)
+#     for i in b:
+#         c.append(i)
+#     print(b)
+#     for i in range(60):
+#         c.append(i)
+#     print(c)
+#     for i in c:
+#         if not isinstance(i,str):
+#             i=i+2
+#             b.append(i)
+#     print(b)
+#
+# func1()
 
-def show(f):
-    def func(*args,**kwargs):
-        start_time = time.time()
-        print(start_time)
-        f(*args,**kwargs)
-        stop_time = time.time()
-        print(stop_time)
-        print(f'Время выполнения функции:{stop_time-start_time}')
-    return func
 
-@show
-def func1():
-    a = ['Volvo','BMW','Audio','Lada','Lexus']
-    b =[]
-    c =[]
-    for i in a:
-        b.append(i)
-    for i in b:
-        c.append(i)
-    print(b)
-    for i in range(60):
-        c.append(i)
-    print(c)
-    for i in c:
-        if not isinstance(i,str):
-            i=i+2
-            b.append(i)
-    print(b)
-
-func1()
