9893d6e23a8d51ed7adfa6d0efb7c490963d0de3
Aleks <dima.aleksin.93@mail.ru>
2022-05-29 16:34:19 +0300

    Промеж фикс

--- lessons/lesson_14/home_job_mpz.py
+++ lessons/lesson_14/home_job_mpz.py
@@ -41,3 +41,18 @@ with myFile:
     writer.writerows(myData)
 
 print("Writing complete")
+
+import csv
+
+header = ['name', 'area', 'country_code2', 'country_code3']
+data = ['Afghanistan', 652090, 'AF', 'AFG']
+
+
+with open('countries.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write the header
+    writer.writerow(header)
+
+    # write the data
+    writer.writerow(data)
