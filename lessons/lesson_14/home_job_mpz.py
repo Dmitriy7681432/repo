2c5fa821e39ccad108d0b3f6e04dac6062e2a716
Aleks <dima.aleksin.93@mail.ru>
2022-06-20 20:31:16 +0300

    Промеж фикс

--- lessons/lesson_14/home_job_mpz.py
+++ lessons/lesson_14/home_job_mpz.py
@@ -6,76 +6,54 @@ import re, csv
 # response = requests.get('url', auth = HTTPBasicAuth('ypur_login', 'your_password'))
 
 # URL = 'https://www.exist.ru/Catalog/Accessory/Cars/'
-URL = 'https://auto.exist.ru/'
+# URL = 'https://auto.exist.ru/'
+cnt=0
+marka = []
 
-page = requests.get(URL)
-
-print(page.status_code)
-# print(page.text)
-soup = BeautifulSoup(page.text, 'html.parser')
-comment = [rev.text for rev in soup.find_all('p', class_ = 'responses-text')]
-# reviews_1 = soup.find_all('div', class_ = 'responses-content')
-
-print(comment)
-print('--'*50)
-# print(marka_1.find_all('div',class_ = 'responses-content'))
-marka_1 = BeautifulSoup(page.text, 'html.parser')
-marka = [hh.h3.text for hh in marka_1.find_all('div',class_ = 'responses-content')]
-print(marka)
-print('--'*50)
-date = [dates.text for dates in soup.find_all(style = 'float: right')]
-print(date)
-print('--'*50)
-#-------------------------------------------------------------------------------------------
-list_temp = []
+while(cnt !=2):
+    if cnt ==0:
+        URL = 'https://auto.exist.ru/otzyvy?page=12'
+    elif cnt ==1:
+        URL = 'https://auto.exist.ru/otzyvy?page=2'
+    page = requests.get(URL)
+    print(page.status_code)
+    # print(page.text)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    comment = [rev.text for rev in soup.find_all('p', class_ = 'responses-text')]
+    # reviews_1 = soup.find_all('div', class_ = 'responses-content')
+    # print(marka_1.find_all('div',class_ = 'responses-content'))
+    marka_1 = BeautifulSoup(page.text, 'html.parser')
+    # marka = [hh.h3.text for hh in marka_1.find_all('div',class_ = 'responses-content')]
+    [marka.append(hh.h3.text) for hh in marka_1.find_all('div',class_ = 'responses-content')]
+    date = [dates.text for dates in soup.find_all(style = 'float: right')]
+    # marka1 = []
+    # for i in marka:
+    #     marka1.append(i)
+    print(marka)
+    cnt = cnt + 1
+    #-------------------------------------------------------------------------------------------
 lst = []
-count =0
-count1 =0
-flag = 0
-for i in marka:
-    i=i+';'
-    list_temp.append(i)
-    for j in date[count:]:
-        if flag ==1:
-            flag = 0
-            break
-        j=j+';'
-        list_temp.append(j)
-        count = count+1
-        for k in comment[count1:]:
-            list_temp.append(k)
-            lst.append(list_temp)
-            list_temp = []
-            count1 = count1 +1
-            flag = 1
-            break
+count =-1
+for i in range(4):
+    count=count+1
+    lst.append([marka[count],';',date[count],';',comment[count]])
 print(lst)
-A = []
-for i in range(3):
-    for j in range(3):
-        A[i][j] = 2
-
-print(A,'AAAA')
 #-------------------------------------------------------------------------------------------
 head_myData = [["auto" ';' "date comment" ';' 'comment']]
-myData =  [['Alex' ';' 'Brian' ';',comment[0]],
-          ['Tom' ';' 'Smith' ';', comment[1]],
-          ['Tom' ';' 'Smith' ';', comment[2]]]
-print(myData)
-
 myFile = open('example2.csv', 'w', newline='')
 # csv.register_dialect('my_dialect',delimiter='\t',doublequote=';',escapechar=':',skipinitialspace='True')
-# csv.register_dialect('my_dialect',delimiter = '\t',doublequote=True)
+csv.register_dialect('my_dialect',delimiter = '\a',doublequote=True)
 with myFile:
     # for data in a:
             # print(data)
             # writer = csv.writer(myFile,quotechar=',', quoting=csv.QUOTE_MINIMAL)
-            writer = csv.writer(myFile,delimiter = '\t')
-            # writer = csv.writer(myFile,'my_dialect')
+            # writer = csv.writer(myFile,delimiter = '\t')
+            writer = csv.writer(myFile,'my_dialect')
             writer.writerows(head_myData)
             writer.writerows(lst)
 print("Writing complete")
-#-------------------------------------------------------------------------------------------
+
+    #-------------------------------------------------------------------------------------------
 
 import csv
 
