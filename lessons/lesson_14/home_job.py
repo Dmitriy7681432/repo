bbc8f3263379f71efc59c2a5f63e37eebaf2dbbe
asus <asus@asus>
2022-06-22 17:01:37 +0300

    Отлаживаю все страницы

--- lessons/lesson_14/home_job.py
+++ lessons/lesson_14/home_job.py
@@ -14,10 +14,11 @@ positive = []
 negative = []
 my_opinion = []
 lst = []
-count1 = 1
+count1 = 71
 count = -1
-URL = 'https://auto.exist.ru/otzyvy?page=1'
-while (count1 < 2):
+flag =0
+URL = 'https://auto.exist.ru/otzyvy?page=71'
+while (count1 < 75):
     print(URL)
     page = requests.get(URL)
     # print(page.status_code)
@@ -25,48 +26,52 @@ while (count1 < 2):
     soup = BeautifulSoup(page.text, 'html.parser')
     # [comment.append(rev.text) for rev in soup.find_all('p', class_='responses-text')]
     for i in soup.find_all('a', class_='readmore'):
-        # print('TTTTT',i.get('href'))
         page1 = requests.get(i.get('href'))
         soup1 = BeautifulSoup(page1.text, 'html.parser')
+        ad = soup1.find('div', class_ = 'error-title')
+        if ad ==True:
+        print(type(ad))
+        try:
+            com = soup1.find('div', class_='review-text').find('div')
+            mpa = dict.fromkeys(range(32))  # Удаление управлящих символов
+            comment.append(com.text.translate(mpa))
 
-        com = soup1.find('div', class_='review-text').find('div')
-        mpa = dict.fromkeys(range(32))  # Удаление управлящих символов
-        comment.append(com.text.translate(mpa))
 
-        pos = soup1.find('div', class_='grid_12 alpha omega blockFormat Positive').find('div', class_='block_item')
-        mpa1 = dict.fromkeys(range(32))  # Удаление управлящих символов
-        positive.append(pos.text.translate(mpa1))
+            pos = soup1.find('div', class_='grid_12 alpha omega blockFormat Positive').find('div', class_='block_item')
+            mpa1 = dict.fromkeys(range(32))  # Удаление управлящих символов
+            positive.append(pos.text.translate(mpa1))
+            # print(pos)
+            neg = soup1.find('div', class_='grid_12 alpha omega blockFormat Negative').find('div', class_='block_item')
+            mpa2 = dict.fromkeys(range(32))  # Удаление управлящих символов
+            negative.append(neg.text.translate(mpa2))
 
-        neg = soup1.find('div', class_='grid_12 alpha omega blockFormat Negative').find('div', class_='block_item')
-        mpa2 = dict.fromkeys(range(32))  # Удаление управлящих символов
-        negative.append(neg.text.translate(mpa2))
+            opin = soup1.find('div', class_='grid_12 alpha omega blockFormat').find('div', class_='block_item')
+            mpa3 = dict.fromkeys(range(32))  # Удаление управлящих символов
+            my_opinion.append(opin.text.translate(mpa3))
+        except AttributeError:
+            print('Отзыв удален')
+            flag =flag +1
 
-        opin = soup1.find('div', class_='grid_12 alpha omega blockFormat').find('div', class_='block_item')
-        mpa3 = dict.fromkeys(range(32))  # Удаление управлящих символов
-        my_opinion.append(opin.text.translate(mpa3))
-    # reviews_1 = soup.find_all('div', class_ = 'responses-content')
-    # print(marka_1.find_all('div',class_ = 'responses-content'))
-    marka_1 = BeautifulSoup(page.text, 'html.parser')
-    [marka.append(hh.h3.text) for hh in marka_1.find_all('div', class_='responses-content')]
+    [marka.append(hh.h3.text) for hh in soup.find_all('div', class_='responses-content')]
     [date.append(dates.text) for dates in soup.find_all(style='float: right')]
-    print(len(comment))
-    print(comment)
-    print(len(marka))
-    print(marka)
-    print(len(date))
-    print(date)
-    print(len(positive))
-    print(positive)
-    print(len(negative))
-    print(negative)
-    print(len(my_opinion))
-    print(my_opinion)
-
+    # print(len(comment))
+    # print(comment)
+    # print(len(marka))
+    # print(marka)
+    # print(len(date))
+    # print(date)
+    # print(len(positive))
+    # print(positive)
+    # print(len(negative))
+    # print(negative)
+    # print(len(my_opinion))
+    # print(my_opinion)
     # -------------------------------------------------------------------------------------------
-    for i in range(5):
+    for i in range(5-flag):
         count = count + 1
         lst.append([marka[count], date[count], comment[count], positive[count], negative[count], my_opinion[count]])
-    print(lst)
+    # print(lst)
+    flag =0
     URL = URL.replace(str(count1), str(count1 + 1))
     count1 = count1 + 1
     # -------------------------------------------------------------------------------------------
