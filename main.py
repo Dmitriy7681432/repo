56e39edc9d581300d012884efe2ddb48283f17fe
Aleks <dima.aleksin.93@mail.ru>
2022-06-25 15:48:24 +0300

    промеж фикс

--- main.py
+++ main.py
@@ -11,8 +11,27 @@ print(c)
 e = []
 d = []
 for i in c:
-    if i!='\n':
+    if i!='\r' or i!='\n':
         e.append(i)
 print(e)
 print(d.append(''.join(e)))
 print(d)
+
+z = []
+r = [i for i in c if i!='\n']
+z.append(''.join(r))
+print(z)
+
+print(c.strip('\\n'))
+
+
+from unicodedata import category
+import unicodedata
+print(category('\r'))
+def remove_control_characters(s):
+    return "".join(ch for ch in s if unicodedata.category(ch)[0]!="C")
+
+a =remove_control_characters(c)
+print(a)
+fa = [ch for ch in c if unicodedata.category(ch)[0]!="C"]
+print(''.join(fa))
