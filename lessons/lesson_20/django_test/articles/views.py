205dffa5d286a4df00c701de364eac480cf04639
Dmitriy <dima@.com>
2023-01-11 23:02:15 +0300

    Начал урок 21

--- lessons/lesson_20/django_test/articles/views.py
+++ lessons/lesson_20/django_test/articles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def main_page(request):
+    return HttpResponse('This is main page')
+
+def article_description(request):
+    return HttpResponse('This is some article')
