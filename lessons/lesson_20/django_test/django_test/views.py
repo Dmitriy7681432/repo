611fcd255263204615c874b9857a012ea05c71b3
Dmitriy <dima@.com>
2023-01-15 23:49:45 +0300

    Промеж фикс урока 21.5

--- lessons/lesson_20/django_test/django_test/views.py
+++ lessons/lesson_20/django_test/django_test/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from articles.models import Article, Tag
-
+import random
 # Create your views here.
 def home_page(request):
-    random_idx = random.randint(0, Article.object.count() - 1)
+    random_idx = random.randint(0, Article.objects.count() - 1)
     # return HttpResponse('This is the home page')
     art_random = get_object_or_404(Article, id=random_idx+1)
     return render(request, 'articles/index.html',{'articles':art_random})
