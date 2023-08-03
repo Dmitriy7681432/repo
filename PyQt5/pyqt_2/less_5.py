acd2b217c4cb7315b30d151d25add15858dc1ab4
Aleksin <dima.aleksin.93@mail.ru>
2023-08-03 16:35:00 +0300

    Разбираю less_5

--- PyQt5/pyqt_2/less_5.py
+++ PyQt5/pyqt_2/less_5.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #5 - Диалоговые окна
 #5.1 - QInputDialog ___________________________________
+"""
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit,
     QInputDialog, QApplication)
@@ -33,7 +34,6 @@ class Example(QWidget):
 
         text, ok = QInputDialog.getText(self, 'Input Dialog',
             'Enter your name:')
-        print(text,"--",ok)
         if ok:
             self.le.setText(str(text))
 
@@ -43,3 +43,106 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
+"""
+#5.2 - QColorDialog ___________________________________
+"""
+import sys
+from PyQt5.QtWidgets import (QWidget, QPushButton, QFrame,
+    QColorDialog, QApplication,QLabel)
+from PyQt5.QtGui import QColor
+from PyQt5 import QtGui
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        col = QColor(0, 0, 0)
+
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(20, 20)
+
+        self.btn.clicked.connect(self.showDialog)
+
+        self.frm = QFrame(self)
+        self.frm.setStyleSheet("QWidget { background-color: %s }"
+            % col.name())
+        self.frm.setGeometry(130, 22, 100, 100)
+
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle('Color dialog')
+        self.show()
+
+
+    def showDialog(self):
+
+        col = QColorDialog.getColor()
+
+        if col.isValid():
+            self.frm.setStyleSheet("QWidget { background-color: %s }"
+                % col.name())
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+"""
+#5.3 - QFontDialog ___________________________________
+import sys
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QPushButton,
+    QSizePolicy, QLabel, QFontDialog, QApplication)
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        vbox = QVBoxLayout()
+
+        btn = QPushButton('Dialog', self)
+        btn.setSizePolicy(QSizePolicy.Fixed,
+            QSizePolicy.Fixed)
+
+        btn.move(20, 20)
+
+        vbox.addWidget(btn)
+
+        btn.clicked.connect(self.showDialog)
+
+        self.lbl = QLabel('Knowledge only matters', self)
+        self.lbl.move(130, 20)
+
+        vbox.addWidget(self.lbl)
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle('Font dialog')
+        self.show()
+
+
+    def showDialog(self):
+
+        font, ok = QFontDialog.getFont()
+        if ok:
+            self.lbl.setFont(font)
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
