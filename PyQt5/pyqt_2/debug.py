0276e5fc87ee5e116bddcf86e092aa46be9d0f21
Aleksin <dima.aleksin.93@mail.ru>
2023-08-05 19:16:27 +0300

    Разбираю less_7

--- PyQt5/pyqt_2/debug.py
+++ PyQt5/pyqt_2/debug.py
@@ -1,34 +1,34 @@
 # -*- coding: utf-8 -*-
-
-class AAA(object):
-    def __init__(self):
-        self.num = "Hello"
-
-    def func_a(self):
-        print("Это метода класса А")
-
-
-class BBB(AAA):
-    # def __init__(self):
-        # super().__init__()
-        # self.num = self.num + "Hello1"
-
-    def func_b(self):
-        print("Это метода класса B")
-
-    def func_a_class(self):
-        self.a = AAA()
-        self.a.func_a()
-
-
-# class CCC(BBB):
 #
-#    def func_c(self):
-#       self.c =BBB.func_a_class(self).func_a()
-#       return self.c
-
-if __name__ == "__main__":
-    a = BBB()
-    text= a.num
-    print(text,"---")
-    # print(a.func_a(),"---")
+# class AAA(object):
+#     def __init__(self):
+#         self.num = "Hello"
+#
+#     def func_a(self):
+#         print("Это метода класса А")
+#
+#
+# class BBB(AAA):
+#     # def __init__(self):
+#         # super().__init__()
+#         # self.num = self.num + "Hello1"
+#
+#     def func_b(self):
+#         print("Это метода класса B")
+#
+#     def func_a_class(self):
+#         self.a = AAA()
+#         self.a.func_a()
+#
+#
+# # class CCC(BBB):
+# #
+# #    def func_c(self):
+# #       self.c =BBB.func_a_class(self).func_a()
+# #       return self.c
+#
+# if __name__ == "__main__":
+#     a = BBB()
+#     text= a.num
+#     print(text,"---")
+#     # print(a.func_a(),"---")
--- PyQt5/pyqt_2/less_6.py
+++ PyQt5/pyqt_2/less_6.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# 6 - Виджеты
+# 6 - Виджеты (часть 1)
 # 6.1 - QCheckBox ______________________________________________
 """
 import sys
@@ -43,6 +43,7 @@ if __name__ == '__main__':
     sys.exit(app.exec_())
 """
 # 6.2 - Кнопка переключателя (toogle button) ______________________________________________
+"""
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton,
     QFrame, QApplication)
@@ -62,7 +63,7 @@ class Example(QWidget):
         self.col = QColor(0, 0, 0)
 
         redb = QPushButton('Red', self)
-        # redb.setCheckable(True)
+        redb.setCheckable(True)
         redb.move(10, 10)
 
         redb.clicked[bool].connect(self.setColor)
@@ -92,7 +93,7 @@ class Example(QWidget):
     def setColor(self, pressed):
 
         source = self.sender()
-
+        print("pressed",pressed)
         if pressed:
             val = 255
         else: val = 0
@@ -113,3 +114,155 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
+"""
+# 6.3 - QSlider (ползунок) ______________________________________________
+"""
+import sys
+from PyQt5.QtWidgets import (QWidget, QSlider,
+    QLabel, QApplication)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        sld = QSlider(Qt.Horizontal, self)
+        sld.setFocusPolicy(Qt.NoFocus)
+        sld.setGeometry(30, 40, 100, 30)
+        sld.valueChanged[int].connect(self.changeValue)
+
+        self.label = QLabel(self)
+        self.label.setPixmap(QPixmap('.//less_6_img/mute.jpg'))
+        self.label.setGeometry(160, 40, 80, 30)
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('QSlider')
+        self.show()
+
+
+    def changeValue(self, value):
+
+        if value == 0:
+            self.label.setPixmap(QPixmap('.//less_6_img/mute.jpg'))
+        elif value > 0 and value <= 30:
+            self.label.setPixmap(QPixmap('.//less_6_img/min.jpg'))
+        elif value > 30 and value < 80:
+            self.label.setPixmap(QPixmap('.//less_6_img/med.jpg'))
+        else:
+            self.label.setPixmap(QPixmap('.//less_6_img/max.jpg'))
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+"""
+# 6.4 - QProgressBar (прогресс бар) ______________________________________________
+"""
+import sys
+from PyQt5.QtWidgets import (QWidget, QProgressBar,
+    QPushButton, QApplication)
+from PyQt5.QtCore import QBasicTimer
+from lessons.lesson_9.dz_class_game_durak import func
+
+
+n = sys._getframe
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.pbar = QProgressBar(self)
+        self.pbar.setGeometry(30, 40, 200, 25)
+
+        self.btn = QPushButton('Start', self)
+        self.btn.move(40, 80)
+        self.btn.clicked.connect(self.doAction)
+
+        self.timer = QBasicTimer()
+        self.step = 0
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('QProgressBar')
+        self.show()
+
+
+    def timerEvent(self, e):
+        if self.step >= 100:
+            self.timer.stop()
+            self.btn.setText('Finished')
+            return
+        self.step = self.step + 1
+        self.pbar.setValue(self.step)
+
+
+    def doAction(self):
+        if self.timer.isActive():
+            self.timer.stop()
+            self.btn.setText('Start')
+        else:
+            self.timer.start(100, self)
+            self.btn.setText('Stop')
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+"""
+# 6.5 - QCalendarWidget (виджет календаря) ______________________________________________
+import sys
+from PyQt5.QtWidgets import (QWidget, QCalendarWidget,
+    QLabel, QApplication)
+from PyQt5.QtCore import QDate
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        cal = QCalendarWidget(self)
+        cal.setGridVisible(True)
+        cal.move(20, 20)
+        cal.clicked[QDate].connect(self.lol)
+
+        self.lbl = QLabel(self)
+        date = cal.selectedDate()
+        self.lbl.setText(date.toString())
+        self.lbl.move(130, 260)
+
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Calendar')
+        self.show()
+
+
+    def showDate(self, date):
+        self.lbl.setText(date.toString())
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
