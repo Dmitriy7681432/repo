d62c45dd09ae8c0654cd4e8b9600c13d20dcb718
aleksin <svitpower@gmil.com>
2023-08-11 23:46:52 +0300

    Рассматриваю мультипроцнссинг

--- logger/project/testing/test.py
+++ logger/project/testing/test.py
@@ -36,14 +36,63 @@ def thread_function(name):
 #     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
 #         executor.map(thread_function, range(3))
 
-from multiprocessing import Pool
+# from multiprocessing import Pool
+#
+# def f(x):
+#     return x+x
+#
+# if __name__ == '__main__':
+#     with Pool(5) as p:
+#         print(p)
+#         print(p.map(f, [1, 2, 3]))
 
-def f(x):
-    return x*x
+# from multiprocessing import Process
+# import os
+#
+# def info(title):
+#     print('module name:', __name__)
+#     print('parent process:', os.getppid())
+#     print('process id:', os.getpid())
+#     print(title)
+#
+# def f(name):
+#     info('function f')
+#     print('hello', name)
+#
+# if __name__ == '__main__':
+#     info('main line')
+#     p = Process(target=f, args=('bob',))
+#     p.start()
+#     p.join()
 
-if __name__ == '__main__':
-    # with Pool(5) as p:
-    #     print(p)
-    #     print(p.map(f, [1, 2, 3]))
-    a =map(f, [1,2,3])
-    print(a)
+# import multiprocessing
+# print("Number of cpu : ", multiprocessing.cpu_count())
+
+from multiprocessing import Process
+
+def print_func(continent='Asia'):
+    print('The name of continent is : ', continent)
+
+if __name__ == "__main__":  # confirms that the code is under main function
+    names = ['America', 'Europe', 'Africa']
+    print('Main')
+    procs = []
+    proc = Process(target=print_func)  # instantiating without any argument
+    procs.append(proc)
+    proc.start()
+
+    # instantiating process with arguments
+    for name in names:
+        print('Start')
+        proc = Process(target=print_func, args=(name,))
+        procs.append(proc)
+        proc.start()
+        print('Start1')
+
+    # complete the processes
+    for proc in procs:
+        print('Join')
+        proc.join()
+
+
+
