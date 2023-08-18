154d6a996acdb5ec07d408ec7aa16d2389c12d50
Aleksin <dima.aleksin.93@mail.ru>
2023-08-18 16:55:44 +0300

    Остановился на уроке координат шаг 31

--- PyQt5/pyqt_3/less_1.py
+++ PyQt5/pyqt_3/less_1.py
@@ -1,35 +1,67 @@
 #  -*- coding:   utf-8  -*-
-from PyQt5 import QtCore, QtGui, QtWidgets
-import time
+# from PyQt5 import QtCore, QtGui, QtWidgets
+# import time
+#
+#
+# class MyWindow(QtWidgets.QPushButton):
+#     def __init__(self):
+#         QtWidgets.QPushButton.__init__(self)
+#         self.setText("Закрыть окно")
+#         self.clicked.connect(QtWidgets.qApp.quit)
+#
+#     def load_data(self, sp):
+#         for i in range(1, 11):  # Имитируем процесс
+#             time.sleep(2)  # Что-то загружаем
+#             sp.showMessage("Загрузка данных... {0}%".format(i * 10),
+#                            QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+#             QtWidgets.qApp.processEvents()  # Запускаем оборот цикла
+#
+#
+# if __name__ == "__main__":
+#     import sys
+#
+#     app = QtWidgets.QApplication(sys.argv)
+#     splash = QtWidgets.QSplashScreen(QtGui.QPixmap("img1.jpeg"))
+#     splash.showMessage("Загрузка данных... 0%",
+#                        QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+#     splash.show()  # Отображаем заставку
+#     QtWidgets.qApp.processEvents()  # Запускаем оборот цикла
+#     window = MyWindow()
+#     window.setWindowTitle("Использование класса QSplashScreen")
+#     window.resize(300, 30)
+#     window.load_data(splash)  # Загружаем данные
+#     window.show()
+#     splash.finish(window)  # Скрываем заставку
+# from PyQt5 import QtGui, QtCore, QtWidgets
+#
+# desktop = QtWidgets.QApplication.desktop()
+# print(desktop.width(),desktop.height())
+# -*- coding: utf-8 -*-
+# from PyQt5 import QtWidgets
+# import sys
+# app = QtWidgets.QApplication(sys.argv)
+# window = QtWidgets.QWidget()
+# window.setWindowTitle("Вывод окна по центру экрана")
+# window.resize(300, 100)
+# desktop = QtWidgets.QApplication.desktop()
+# x = (desktop.width() - window.width()) // 2
+# y = (desktop.height() - window.height()) // 2
+# window.move(x, y)
+# window.show()
+# sys.exit(app.exec_())
 
-
-class MyWindow(QtWidgets.QPushButton):
-    def __init__(self):
-        QtWidgets.QPushButton.__init__(self)
-        self.setText("Закрыть окно")
-        self.clicked.connect(QtWidgets.qApp.quit)
-
-    def load_data(self, sp):
-        for i in range(1, 11):  # Имитируем процесс
-            time.sleep(2)  # Что-то загружаем
-            sp.showMessage("Загрузка данных... {0}%".format(i * 10),
-                           QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom, QtCore.Qt.white)
-            QtWidgets.qApp.processEvents()  # Запускаем оборот цикла
-
-
-if __name__ == "__main__":
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    splash = QtWidgets.QSplashScreen(QtGui.QPixmap("img1.jpeg"))
-    splash.showMessage("Загрузка данных... 0%",
-                       QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom, QtCore.Qt.white)
-    splash.show()  # Отображаем заставку
-    QtWidgets.qApp.processEvents()  # Запускаем оборот цикла
-    window = MyWindow()
-    window.setWindowTitle("Использование класса QSplashScreen")
-    window.resize(300, 30)
-    window.load_data(splash)  # Загружаем данные
-    window.show()
-    splash.finish(window)  # Скрываем заставку
-    sys.exit(app.exec_())
+# Вывод окна точно по центру экрана
+# -*- coding: utf-8 -*-
+from PyQt5 import QtWidgets
+import sys
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QWidget()
+window.setWindowTitle("Вывод окна по центру экрана")
+window.resize(300, 100)
+# window.move(window.width() * -2, 0)
+window.show()
+desktop = QtWidgets.QApplication.desktop()
+x = (desktop.width() - window.frameSize().width()) // 2
+y = (desktop.height() - window.frameSize().height()) // 2
+window.move(x, y)
+sys.exit(app.exec_())
