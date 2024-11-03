7044600d9df4bf04acf3f6197c33fa485f8bf177
aleksin <svitpower@gmil.com>
2024-11-03 23:44:21 +0300

    Калибратор. Создал некоторые функции для преобзавония в число и в байты

--- calibrator/read_data.py
+++ calibrator/read_data.py
@@ -1,4 +1,4 @@
-import serial,time
+import serial,time,binascii,struct
 import serial.tools.list_ports
 
 def can_open(arg):
@@ -21,17 +21,30 @@ def can_close(arg):
     print(msg,'can_close')
     return 0
 
-ports = serial.tools.list_ports.comports()
+def trasformed_in_value(arg):
+    arg = arg[13:21]
+    arg = arg[6:8]+arg[4:6]+arg[2:4]+arg[0:2]
+    arg = arg.decode('utf-8')
+    arg = struct.unpack('!I', bytes.fromhex(arg))
+    return arg[0]
 
-for port in ports:
-    print(port.device)
+def transformed_in_bytes(arg):
+    arg= hex(arg)[2:].upper()
+    arg = arg[6:8] + arg[4:6] + arg[2:4] + arg[0:2]
+    arg = arg.encode('utf-8')
+    arg = b't64A800000000' + arg
+    return arg
+# ports = serial.tools.list_ports.comports()
+#
+# for port in ports:
+#     print(port.device)
 
 port = "COM88"  # Replace with the appropriate COM port name
 baudrate = 3000000 # Replace with the desired baud rate
 count = 0
 list_read_data = []
-with serial.Serial(port, baudrate=baudrate,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS) as ser:
-   can_open(ser)
+# with serial.Serial(port, baudrate=baudrate,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS) as ser:
+#    can_open(ser)
    # with open('read_data.txt','wb') as f:
    #     while True:
    #         count +=1
@@ -46,18 +59,18 @@ with serial.Serial(port, baudrate=baudrate,stopbits=serial.STOPBITS_ONE,bytesize
    #             print(read_data)
    #             print(value)
    #             break
-   can_close(ser)
+   # can_close(ser)
+
 
 b=b't0338002F00000000D4BF0CC3'
-# b = b't0088EB2E0000245600000380\nt0338002F00000000D4BF03E7\nt0338002F00000000D4BF03E8\nt0338002F00000000D4BF0'
-b = b[13:21]
+print(hex(b[0]))
+b = trasformed_in_value(b)
 print(b)
-b = b[6:8]+b[4:6]+b[2:4]+b[0:2]
-b = b.decode('utf-8')
-print(b)
-import  struct
-b = struct.unpack('i', bytes.fromhex(b))
-print(b)
+while count <20:
+    count += 1
+    b +=4
+    a = transformed_in_bytes(b)
+    print(a)
 
 # a = b'A8\r'
 # # a = a.lstrip(b'\r')
