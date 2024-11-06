5fbfa2b9d51b0004639fb6462ecfe84db154aaf7
Acer <Acer@.com>
2024-11-06 16:32:48 +0300

    Калибратор. Код написан, пытаюсь решить проблему ускорения обсчета данных

--- calibrator/read_data.py
+++ calibrator/read_data.py
@@ -4,20 +4,31 @@ from lxml import etree
 from debug import printf
 
 
-def can_open(arg):
+def can_open_O(arg):
     arg.timeot = 0.1
     msg = b"C\r"
     arg.write(msg)
     # printf("T>", msg)
     # print("R>", arg.read(1000))
-    msg = b"S5\rZ1\r"
+    msg = b"S8\rZ1\r"
+    arg.write(msg)
+    # printf("T>", msg)
+    # print("R>", arg.read(1000))
+    msg = b"Y\r"
+    arg.write(msg)
+
+def can_open_L(arg):
+    arg.timeot = 0.1
+    msg = b"C\r"
+    arg.write(msg)
+    # printf("T>", msg)
+    # print("R>", arg.read(1000))
+    msg = b"S8\rZ1\r"
     arg.write(msg)
     # printf("T>", msg)
     # print("R>", arg.read(1000))
     msg = b"L\r"
     arg.write(msg)
-    return 0
-
 
 def can_close(arg):
     msg = b"C\r"
@@ -33,6 +44,12 @@ def transformed_in_value(arg):
     arg = struct.unpack('!I', bytes.fromhex(arg))
     return arg[0]
 
+def transformed_in_address(arg):
+    arg = arg[5:13]
+    arg = arg[6:8] + arg[4:6] + arg[2:4] + arg[0:2]
+    arg = arg.decode('utf-8')
+    arg = struct.unpack('!I', bytes.fromhex(arg))
+    return arg[0]
 
 def transformed_in_bytes(arg):
     arg = hex(arg)[2:].upper()
@@ -41,47 +58,38 @@ def transformed_in_bytes(arg):
     arg = b't618800000000' + arg
     return arg
 
-
-def parse_preset(xmlFile, arg):
-    value = 0
-    doc = etree.parse(xmlFile)
-    for setting in doc.findall('.//can'):
-        number = setting.attrib.get('number')
-        for products in setting.findall('products/'):
-            product = products.tag
-            if product == 'SES200M':
-                cb = products.attrib.get('cb')
-                if cb == 'BU_50':
-                    printf(number)
-                    arg += 4
-
-
 # ports = serial.tools.list_ports.comports()
 #
 # for port in ports:
 #     print(port.device)
 
 def main():
+    start_time = time.time()
     port = "COM88"  # Replace with the appropriate COM port name
     baudrate = 3000000  # Replace with the desired baud rate
     count = 0
     list_read_data = []
     value = 0
-    with serial.Serial(port, baudrate=baudrate, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS) as ser:
-        can_open(ser)
+    flag =0
+    # with serial.Serial(port, baudrate=baudrate, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS) as ser:
+    with serial.Serial(port, baudrate=baudrate) as ser:
+        ser.timeout =0.1
+        can_open_O(ser)
         while True:
-            read_data = ser.read(100)
-            read_data = read_data.replace(b'\r', b'\n')
-            if b'033' in read_data and b'002F0000' in read_data:
-                list_read_data = read_data.split(b'\n')
+            read_data = ser.read(27)
+            # read_data = read_data.replace(b'\r', b'\n')
+            if b't033' in read_data and b'002F0000' in read_data:
+                list_read_data = read_data.split(b'\r')
                 for i in list_read_data:
-                    if b'033' in i and b'002F0000' in i and len(i) > 21:
+                    if b't033' in i and b'002F0000' in i and len(i) > 21:
                         read_data = i
                         value = transformed_in_value(read_data)
                         printf(read_data)
                         printf(value)
+                        flag =1
                         break
-                break
+                if flag ==1:
+                    flag =0;break
 
         with open('read_data.txt', 'wb') as f:
             while True:
@@ -89,24 +97,34 @@ def main():
                 value+=4
                 count +=1
                 printf(msg_bytes)
-                f.write(msg_bytes+b'\n')
+                # f.write(msg_bytes+b'\t')
+                # ser.timeout = 0.01
+                msg = b"Y\r"
+                ser.write(msg)
                 ser.write(msg_bytes)
                 # time.sleep(1)
                 while True:
-                    read_data = ser.read(100)
-                    read_data = read_data.replace(b'\r', b'\n')
+                    read_data = ser.read(27)
+                    printf(read_data)
+                    # printf()
+                    # read_data = read_data.replace(b'\r', b'\n')
                     if b't64A' in read_data:
-                        list_read_data = read_data.split(b'\n')
+                        list_read_data = read_data.split(b'\r')
                         for i in list_read_data:
                             if i[1:4] == b'64A' in i and len(i) > 21:
                                 read_data = i
                                 printf(read_data)
-                                f.write(read_data+b'\n')
-                                address = transformed_in_value(read_data)
-                                print(address)
-                                f.write(hex(address).encode('utf-8')+b'\n')
+                                # f.write(read_data+b'\n')
+                                can_value = transformed_in_value(read_data)
+                                can_address = transformed_in_address(read_data)
+                                print(can_value)
+                                print(can_address)
+                                f.write(hex(can_address).encode('utf-8')+b'\t')
+                                f.write(hex(can_value).encode('utf-8')+b'\n')
+                                flag =1
                                 break
-                        break
+                        if flag ==1:
+                            flag =0;break
                 if count ==7: break
             doc = etree.parse('params.xml')
             for setting in doc.findall('.//setting'):
@@ -120,28 +138,37 @@ def main():
                             msg_bytes = transformed_in_bytes(value)
                             value += 4
                             printf(msg_bytes)
-                            f.write(msg_bytes+b'\n')
+                            # f.write(msg_bytes+b'\t')
+                            # ser.timeout = 0.1
+                            msg = b"Y\r"
+                            ser.write(msg)
                             ser.write(msg_bytes)
                             # time.sleep(1)
                             while True:
-                                # can_open(ser)
-                                msg = b"L\r"
-                                ser.write(msg)
-                                read_data = ser.read(100)
-                                read_data = read_data.replace(b'\r', b'\n')
+                                read_data = ser.read(27)
+                                printf(read_data)
+                                # printf()
+                                # read_data = read_data.replace(b'\r', b'\n')
                                 if b't64A' in read_data:
-                                    list_read_data = read_data.split(b'\n')
+                                    list_read_data = read_data.split(b'\r')
                                     for i in list_read_data:
                                         if i[1:4] == b'64A' and len(i) > 21:
                                             read_data = i
                                             printf(read_data)
-                                            f.write(read_data+b'\n')
-                                            address = transformed_in_value(read_data)
-                                            printf(address)
-                                            f.write(hex(address).encode('utf-8')+b'\n')
+                                            # f.write(read_data+b'\n')
+                                            can_value= transformed_in_value(read_data)
+                                            can_address = transformed_in_address(read_data)
+                                            printf(can_value)
+                                            f.write(hex(can_address).encode('utf-8')+b'\t')
+                                            f.write(hex(can_value).encode('utf-8')+b'\n')
+                                            flag =1
                                             break
-                                    break
+                                    if flag ==1:
+                                        flag =0;break
 
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print('Elapsed time: ', elapsed_time)
         can_close(ser)
 
 if __name__ == '__main__':
--- calibrator/test.py
+++ calibrator/test.py
@@ -2,15 +2,16 @@ import serial, time, binascii, struct
 import serial.tools.list_ports
 from lxml import etree
 from debug import printf
-from read_data import transformed_in_value
+from read_data import transformed_in_value,transformed_in_address
 # b = b't64A8E400000000E50000'
 # b = b't64A8E400000000E500009'
-b = b'AAB8E400000000E5000064A8\nA64A8E400000000E50000930D'
-if b't64A' in b:
-# b = b[1:4]
-    print(b)
-# b = transformed_in_value(b)
-# print(b)
+# b = b'AAB8E400000000E5000064A8\nA64A8E400000000E50000930D'
+b = b't61880400D4BF5A5AA5A5'
+c = b't6188000000000401D4BF'
+b = transformed_in_address(b)
+c = transformed_in_value(c)
+print(hex(b))
+print(hex(c))
 # port = "COM88"  # Replace with the appropriate COM port name
 # baudrate = 3000000  # Replace with the desired baud rate
 # count = 0
