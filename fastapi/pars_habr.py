154c27ab6a475339131d459811be860a00f728b9
Aleksin <dima.aleksin.93@mail.ru>
2026-01-14 16:48:56 +0300

    Доп задание по парсингу хабра

--- fastapi/pars_habr.py
+++ fastapi/pars_habr.py
@@ -2,6 +2,8 @@
 import requests, io, sys
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
+from pprint import pprint
 
 main_page = "https://habr.com/ru/articles/top/daily/"
 
@@ -25,20 +27,37 @@ def get_soup(html_text: str) -> BeautifulSoup:
     return BeautifulSoup(html_text, features='lxml')
 
 
-def get_all_habr_posts(soup: BeautifulSoup):
-    all_arcticles_soup = soup.find_all("a", class_='tm-title__link')
-    for article_soup in all_arcticles_soup:
-        article_title: str = article_soup.find('span').text
-        print(f"{article_title=}")
-        rating = article_soup.find('span',class_='tm-icon-counter__value')
-        print(f"{rating=}")
+@dataclass
+class ArticleData:
+    title: str
+    views: str
+    url: str
 
+    def __repr__(self):
+        return f'{__class__.__name__}. Title: {self.title}, Views: {self.views}, URL: {self.url}'
+
+
+def get_all_habr_posts(soup: BeautifulSoup) -> list[ArticleData]:
+    posts_data = []
+    all_arcticles_soup = soup.find_all("article", class_='tm-articles-list__item')
+    for article_soup in all_arcticles_soup:
+        article_title: str = article_soup.find('a', class_="tm-title__link").find('span').text
+        article_views: str = article_soup.find('span', class_='tm-icon-counter__value').text
+        article_url: str = article_soup.find('a')
+        posts_data.append(ArticleData(
+           title=article_title,
+           views=article_views,
+           url=article_url
+        ))
+    return posts_data
 
 
 def main():
     html = get_url_html(main_page)
     soup = get_soup(html)
-    get_all_habr_posts(soup)
+    posts = get_all_habr_posts(soup)
+    pprint(posts)
+    # print(f"{posts=}")
 
 
 if __name__ == "__main__":
